c383a8b5727bf8d8b973385d3c4631a2ed3d8ae5
Angie Goza <angielg9888@gmail.com>
2018-11-20 18:56:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,9 @@
 def key_for_min_value(name_hash)
     name_hash.collect do |key, value|
       if value < value
-      key
+        if value < value
+          key
+        end
+      end
     end
-  end
 end
